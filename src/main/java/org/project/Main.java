994ddf5dc41c4948ae358a1c80445b50e5965c0f
PogozhevIlya <ilya_pogozhev00@mail.ru>
2022-12-11 15:36:27 +0300

    fix changes, add input/output data

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -1,16 +1,22 @@
 package org.project;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.project.entity.Show;
 import org.project.entity.Storage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
     static Storage storage = Storage.getInstance () ;
 
-    public static void main ( String [] args ) {
+    public static void main ( String [] args ) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String filePath = "./dbJson.json";
         Show[] shows = storage.findAllShows () ;
-        for ( int i = 0 ; i < shows.length; i++ ) {
-            Show show = shows [ i ] ;
-            System.out.println ( show ) ;
+        for (Show show : shows) {
+            System.out.println(show);
         }
+        objectMapper.writeValue(new File(filePath), shows);
     }
 }
--- src/test/java/org/project/entity/MainTest.java
+++ src/test/java/org/project/entity/MainTest.java
@@ -1,7 +1,5 @@
 package org.project.entity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MainTest {
-
-}
+//public class MainTest { }
