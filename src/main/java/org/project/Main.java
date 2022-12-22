8b72d122bcf939f5c94682d910e0918186112cef
PogozhevIlya <ilya_pogozhev00@mail.ru>
2022-12-22 16:26:15 +0300

    fix changes, add input/output data

--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.project.entity.Show;
 import org.project.entity.Storage;
 
+import java.util.Arrays;
+import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+
 
 public class Main {
     static Storage storage = Storage.getInstance () ;
@@ -18,5 +21,30 @@ public class Main {
             System.out.println(show);
         }
         objectMapper.writeValue(new File(filePath), shows);
+
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Функциональщина:\n1- Найти название фильма по id,  2- показ по названию, 3 - показ по id, 4- Вывести все фильмы в прокате\n");
+            int choice = in.nextInt();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Введите id фильма ");
+                    int id_film = in.nextInt();
+                    System.out.println(Storage.getInstance().findMovieById(id_film));
+                }
+                case 2 -> {
+                    System.out.println("Введите название фильма ");
+                    String title_film = in.nextLine();
+                    System.out.println(Storage.getInstance().findMovieByTitle(title_film));
+
+                }
+                case 3 -> {
+                    System.out.println("Введите id фильма ");
+                    int id_show = in.nextInt();
+                    System.out.println(Storage.getInstance().findShowById(id_show));
+                }
+                case 4 -> System.out.println(Arrays.toString(Storage.getInstance().findAllMovies()));
+            }
+        }
     }
 }
--- src/main/java/org/project/entity/Storage.java
+++ src/main/java/org/project/entity/Storage.java
@@ -1,5 +1,7 @@
 package org.project.entity;
 
+import java.util.Objects;
+
 public class Storage {
     private static Storage ourInstance;
     private final Movie [] movies;
@@ -13,29 +15,30 @@ public class Storage {
     }
 
     private Storage () {
-        movies = new Movie [] { new Movie ( 1 , "Return of the JNDI" ) ,
-                new Movie ( 2 , "A Bug's Life" ) , new Movie ( 3 , "Fatal Exception" ) ,
+        movies = new Movie [] {
+                new Movie ( 1 , "Return of the JNDI" ) ,
+                new Movie ( 2 , "A Bug's Life" ) ,
+                new Movie ( 3 , "Fatal Exception" ) ,
                 new Movie ( 4 , "Silence of the LANs" ) ,
                 new Movie ( 5 , "Object of my Affection" ) } ;
-        shows = new Show [] { new Show ( 1 , movies [ 0 ] , "5:30pm" , 100 ) ,
+        shows = new Show [] {
+                new Show ( 1 , movies [ 0 ] , "5:30pm" , 100 ) ,
                 new Show ( 2 , movies [ 0 ] , "7:00pm" , 300 ) ,
                 new Show ( 3 , movies [ 1 ] , "6:00pm" , 200 ) ,
                 new Show ( 4 , movies [ 4 ] , "9:00pm" , 200 ) } ;
     }
 
     public synchronized Movie findMovieById ( int id ) {
-        for ( int i = 0 ; i < movies.length; i++ ) {
-            Movie movie = movies [ i ] ;
-            if ( movie.id == id )
+        for (Movie movie : movies) {
+            if (movie.id == id)
                 return movie;
         }
         return null ;
     }
 
     public synchronized Movie findMovieByTitle ( String title ) {
-        for ( int i = 0 ; i < movies.length; i++ ) {
-            Movie movie = movies [ i ] ;
-            if ( movie.title == title )
+        for (Movie movie : movies) {
+            if (Objects.equals(movie.title, title))
                 return movie;
         }
         return null ;
@@ -46,18 +49,16 @@ public class Storage {
     }
 
     public synchronized Show findShowById ( int id ) {
-        for ( int i = 0 ; i < shows.length; i++ ) {
-            Show show = shows [ i ] ;
-            if ( show.id == id )
+        for (Show show : shows) {
+            if (show.id == id)
                 return show;
         }
         return null ;
     }
 
     public synchronized Show findShowByMovie ( Movie movie ) {
-        for ( int i = 0 ; i < shows.length; i++ ) {
-            Show show = shows [ i ] ;
-            if ( show.movie.id == movie.id )
+        for (Show show : shows) {
+            if (show.movie.id == movie.id)
                 return show;
         }
         return null ;
